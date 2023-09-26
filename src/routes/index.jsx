aa8824d7e4d958403b6e113b3d4572a7bb732783
bhumika-aga <bhumika.aga@gmail.com>
2023-09-26 23:15:13 +0530

    Final Changes

---
+++ src/routes/index.jsx
@@ -0,0 +1,13 @@
+export default function Index() {
+    return (
+        <p id="zero-state">
+            This is a demo for React Router.
+            <br />
+            Check Out{" "}
+            <a href="https://reactrouter.com">
+                The Docs at reactrouter.com
+            </a>
+            .
+        </p>
+    );
+}
